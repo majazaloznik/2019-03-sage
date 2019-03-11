4dec5914c1acd24d2838ecff8a7cd7ce37f06a91
majazaloznik <maja.zaloznik@gmail.com>
2019-03-11 17:03:39 +0200

    Complete draft
    Working draft presentation
    Complete scratchpad

--- code/scratchpad.R
+++ code/scratchpad.R
@@ -1,5 +1,4 @@
 # R basics ####################################################################
-
 # calculation  #########
 
 7+3
@@ -12,52 +11,188 @@
 
 # assigning variables #########################################################
 
-a <- 12
-b <- 4
-
-(a * b)*2
+height <- 11
+width <- 5
+height
+width
+area <- height * width
+area 
+height <- 7
+area
+area <- height * width
+area
 
 # data types ##################################################################
-## numeric vectors
-vec <- c(1,2,3,4,8)
-vec2 <- c(9,1,2)
-
-vec + vec2
-
-vec*10
-## string vectors
-string <- c("maja", "michael", "amy", "nabila", "taylor")
-
-## logical vectors
-
-vec3 <- c(TRUE, FALSE, FALSE, TRUE, TRUE)
-
+# numeric vectors
+vec1 <- c(20, 33, 41, 22, 12)
+vec1
+vec2 <- 1:5
+vec2
+vec1 + vec2
+vec1 * 10
+vec2 + 1000
+# string  vectors
+vec3 <- c("Maja", "Michael", "Amy", "Nabila", "Taylor")
+vec3
+# logical vectors 
+vec4 <- c(TRUE, FALSE, FALSE, TRUE, TRUE)
+vec4
 ## mixing data types
 
-vec4 <- c(1, 2, 3, "four", "five")
-vec4  
+vec5 <- c(1, 2, 3, "four", "five")
+vec5  
+
 
 ## functions #########################
 
-c(1,2 4))
+# calculate the average
+(20 +33 + 41 + 22 + 12)/5
+sum(vec1)/5
+sum(vec1)/length(vec1)
+mean(vec1)
 
-(1 + 2+ 3+ 4+ 8)/5)
-
-
-sum(vec)/5
-
-sum(vec)/length(vec)
-
-mean(vec)
-
-rnorm(100, 10)
-
-rnor
+# sample from normal distribution
+rnorm(10)
+rnorm(10, 5)
+rnorm(10, 5, 100)
 
 ## user written function
-fair.dice <- function(x){sample(1:x, 1)}
+# user defined funcitons
+sample(1:6, 1)
+dice <- function(){sample(1:6, 1)}
+dice()
 
-fair.dice(100)
+dice.new <- function(sides){sample(1:sides, 1)}
+dice.new(3)
+dice.new(100)
   
-  
-  
+## data frames 
+
+# create data frame
+vec5 <- c(1981, 1974, 1993, 1957, 1971)
+
+data.frame(vec1, vec2, vec3, vec4, vec5)
+vec5 <- c(1981, 1974, 1993, 1957, 1971)
+
+my.df <- data.frame(id = vec2, 
+                    year.born = vec5, 
+                    name = vec3, 
+                    rps = vec1,  
+                    like.wc = vec4)
+my.df
+
+## plotting 
+
+plot(my.df$rps,
+     type = "p",
+     col = "red",
+     pch = 19)
+
+hist(my.df$rps)
+
+
+plot(my.df$year.born, my.df$rps,
+     col = "red",
+     xlab = "Year born",
+     ylab = "Rock-Paper-Scissors score")
+
+
+# linear regression
+
+# restart R session
+# load data
+# install.packages("gapminder")
+library(gapminder)
+head(gapminder)
+
+# inspect data
+unique(gapminder$country)
+min(gapminder$year)
+max(gapminder$year)
+
+# plot data
+plot(jitter(gapminder$year), gapminder$lifeExp)
+
+
+# regress 
+
+reg1 <- lm(lifeExp ~ year, data = gapminder)
+
+summary(reg1)
+
+# plot abline  reg1
+
+plot(jitter(gapminder$year), gapminder$lifeExp)
+abline(reg1$coefficients, col = "red", lwd = 3)
+
+# regression 2 
+
+reg2 <- lm(lifeExp ~ gdpPercap, data = gapminder)
+
+summary(reg2)
+
+
+plot(gapminder$gdpPercap, gapminder$lifeExp)
+abline(reg2, col = "red", lwd = 3)
+
+# regression 3
+
+reg3 <- lm(lifeExp ~ log(gdpPercap), data = gapminder)
+
+summary(reg3)
+plot(log(gapminder$gdpPercap), gapminder$lifeExp)
+abline(reg3, col = "red", lwd = 3)
+
+# multiple regression 
+
+reg4 <- lm(lifeExp ~ log(gdpPercap) + year, data = gapminder)
+
+summary(reg4)
+
+# mulitple regression with categorical variable
+gapminder.2007 <- subset(gapminder, year == 2007)
+reg5 <- lm(lifeExp ~ log(gdpPercap)  + continent, data = gapminder.2007)
+
+summary(reg5)
+coef <- reg5$coefficients
+
+plot(log(gapminder.2007$gdpPercap), gapminder.2007$lifeExp, 
+     col =gapminder.2007$continent, pch = 19)
+
+abline(coef[1], coef[2], col = "black")
+abline(coef[1]+ coef[3], coef[2], col = "red")
+abline(coef[1]+ coef[4], coef[2], col = "green3")
+abline(coef[1]+ coef[5], coef[2], col = "blue")
+abline(coef[1]+ coef[6], coef[2], col = "cyan")
+
+legend("topleft", legend = c("Africa", "Americas", "Asia", "Europe", "Oceania"),
+       col = 1:5, pch = 19)
+
+# mulitple regression with categorical variable and interaction
+reg6 <- lm(lifeExp ~ log(gdpPercap) * continent, data = gapminder.2007)
+
+summary(reg6)
+
+plot(log(gapminder.2007$gdpPercap), gapminder.2007$lifeExp, 
+     col = gapminder.2007$continent, pch = 19)
+
+abline(reg6$coefficients[1], reg6$coefficients[2])
+abline(reg6$coefficients[1] + reg6$coefficients[3], 
+       reg6$coefficients[2] + reg6$coefficients[7], col = "green3")
+
+abline(reg6$coefficients[1] + reg6$coefficients[4], 
+       reg6$coefficients[2] + reg6$coefficients[8], col = "red")
+
+abline(reg6$coefficients[1] + reg6$coefficients[5], 
+       reg6$coefficients[2] + reg6$coefficients[9], col = "blue")
+
+abline(reg6$coefficients[1] + reg6$coefficients[6], 
+       reg6$coefficients[2] + reg6$coefficients[10], col = "cyan")
+
+legend("topleft", legend = c("Africa", "Americas", "Asia", "Europe", "Oceania"),
+       col = 1:5, pch = 19)
+
+library(effects)
+interaction <- effect("log(gdpPercap) * continent", reg6)
+plot(interaction, style = "line", multiline = TRUE)
+
